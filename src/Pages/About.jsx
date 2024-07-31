987ff62b624ca60e8b6514674022841201806b3b
swapnil_ranjan <swapnilranjan181200@gmail.com>
2024-07-31 12:38:22 +0530

    about page added

---
+++ src/Pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './Home';
+
+const About = () => {
+  return (
+    <div>
+      <div>About</div>
+      <div className="me">
+        <h1>I am Swapnil Ranjan</h1>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../Pages/Home";
+import About from '../Pages/About';
 
 const Routers = () => {
   return (
     <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
     </Routes>
   )
 }
